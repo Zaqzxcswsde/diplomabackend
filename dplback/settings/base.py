d711e7a65985c54194fd6f00965c07f9fb360b97
Mikhail Ivanov <zaqzxcswsde@gmail.com>
2025-03-27 05:14:35 +0300

    fix user not yet logged 500 bug

--- dplback/settings/base.py
+++ dplback/settings/base.py
@@ -169,6 +169,8 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_METADATA_CLASS': None,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
