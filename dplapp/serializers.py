2cbf79dbe88afc0edc5c022d5c421ac40698e087
Mikhail Ivanov <zaqzxcswsde@gmail.com>
2025-04-06 09:35:36 +0300

    fix 500 if no public_key key given

--- dplapp/serializers.py
+++ dplapp/serializers.py
@@ -411,12 +411,13 @@ class MainRequestSerializer(serializers.Serializer):
 
         # field_names = [self.get_fields().keys())
 
-        self.context['public_key'] = claimed_payload['public_key']
 
         if not set(claimed_payload.keys()) == (set(self.get_fields().keys()) - {'ip'}):
             raise serializers.ValidationError({
                 api_settings.NON_FIELD_ERRORS_KEY: ERRORS[ErrorCodes.INCORR_TOKEN_KEYS] + "Got {claimed_payload.keys()=}, but expected {self.fields.keys()=}"
             })
+        
+        self.context['public_key'] = claimed_payload['public_key']
         
         app_ver = settings.APP_VERSION
         if claimed_payload['version'] != app_ver:
